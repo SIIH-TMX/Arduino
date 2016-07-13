466a02a995224150752198b9781b4c73ad8049bc
SIIH-TMX <siih.tmx@gmail.com>
2016-07-13 06:06:49 -0500

    Add files via upload

---
+++ pir/pir.ino
@@ -0,0 +1,24 @@
+
+
+const byte pirPin = 2;
+const byte led = 5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(led, OUTPUT);
+    attachInterrupt(digitalPinToInterrupt(pirPin), detectado,RISING  );
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void detectado(){
+
+digitalWrite(led, !digitalRead(led));
+  
+}
+

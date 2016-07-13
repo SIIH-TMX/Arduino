93a6c20aaabe0e5ee21cd7729fe3f1ee41ed3c50
SIIH-TMX <siih.tmx@gmail.com>
2016-07-13 06:45:12 -0500

    Add files via upload

---
+++ l298/l298.ino
@@ -0,0 +1,38 @@
+#define velBaja 85
+#define velMedia 170
+#define velAlta= 255
+
+byte motorI = 5 , motorD=6;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(motorI,OUTPUT);
+pinMode(motorD,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+giroDerecha(velBaja);
+delay(1000);
+giroIzquierda(velMedia);
+delay(1000);
+paro();
+}
+
+
+void giroDerecha(byte velocidad){
+  digitalWrite(motorD,LOW);
+  analogWrite(motorI,velocidad);
+}
+void giroIzquierda(byte velocidad){
+    digitalWrite(motorI,LOW);
+  analogWrite(motorD,velocidad);
+  
+}
+
+void paro(){
+  digitalWrite(motorD,LOW);
+  digitalWrite(motorI,LOW);
+}
+
